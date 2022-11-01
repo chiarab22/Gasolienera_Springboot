28b0cb3da95728bd8ba9e3487acb18da68e4fcf4
chiarab22 <cbocc@myuax.com>
2022-11-01 14:48:34 +0100

    Proyecto Springboot creado

---
+++ gasolineraspringboot/src/main/java/com/example/gasolineraspringboot/GasolineraSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gasolineraspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasolineraSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasolineraSpringBootApplication.class, args);
+	}
+
+}
