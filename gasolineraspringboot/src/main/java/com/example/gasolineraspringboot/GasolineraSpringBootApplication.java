a3be43ef6239e5f79d7beaaed238cdb157662026
chiarab22 <cbocc@myuax.com>
2022-11-01 16:03:09 +0100

    cambios gasolinera

--- gasolineraspringboot/src/main/java/com/example/gasolineraspringboot/GasolineraSpringBootApplication.java
+++ gasolineraspringboot/src/main/java/com/example/gasolineraspringboot/GasolineraSpringBootApplication.java
@@ -1,7 +1,6 @@
 package com.example.gasolineraspringboot;
 
 import com.example.gasolineraspringboot.gasolinera.Lanzador;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
